eba62528268398d51ee7dc4d7bb9381eb72e4050
Darshan <darshan@livr.co>
2025-12-09 14:59:59 +0000

    Update on the renderer with link

--- libs/three.js
+++ libs/three.js
@@ -1,7 +1,7 @@
 import { Matrix4, Vector3, Quaternion, Scene, WebGLRenderer, PerspectiveCamera, Group, sRGBEncoding } from "./three.min.js";
 import * as tf from 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.9.0/dist/tf.min.js';
 //import { CSS3DRenderer } from '../libs/CSS3DRenderer.js';
-import {CSS3DRenderer} from 'three/addons/renderers/CSS3DRenderer.js'
+import { CSS3DRenderer } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.min.js'
 import { Controller } from "./controller.js";
 import { UI } from "./ui.js";
 
